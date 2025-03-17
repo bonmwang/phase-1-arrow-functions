4be2884565646228eacfd3bd4d26f6af710f8a69
Boniface <bm715452@gmail.com>
2025-03-17 15:20:50 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1. Function expression called `divide`
+const divide = function () {
+    return 2000 / 100;
+};
+
+// 2. Arrow function called `square`
+const square = (x) => x * x;
+
+// 3. Arrow function called `add`
+const add = (a, b) => a + b;
+
+console.log(divide()); // Output: 20
+console.log(square(5)); // Output: 25
+console.log(add(10, 20)); // Output: 30
